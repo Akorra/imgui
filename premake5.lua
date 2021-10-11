c6ce543723fbc590e1c8655932d90c70a2ab3c56
Akorra <neverfearphilishere@gmail.com>
2021-10-11 15:44:27 +0100

    lua

--- premake5.lua
+++ premake5.lua
@@ -10,8 +10,9 @@ project "ImGui"
 		"imconfig.h",
 		"imgui.h",
 		"imgui.cpp",
-		"imgui_draw.cpp",
 		"imgui_internal.h",
+		"imgui_draw.cpp",
+        "imgui_tables.cpp",
 		"imgui_widgets.cpp",
 		"imstb_rectpack.h",
 		"imstb_textedit.h",
@@ -32,8 +33,10 @@ project "ImGui"
 
 	filter "configurations:Debug"
 		runtime "Debug"
+		buildoptions "/MDd"
 		symbols "on"
 
 	filter "configurations:Release"
 		runtime "Release"
+		buildoptions "/MD"
 		optimize "on"
